74dc55e3ee898bac9932bb48a72d2a9dc9bfd176
Nathan Ngo <ngothuan2422001@gmail.com>
2025-12-21 18:18:51 +0700

    chore: Add new image assets for the project

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -1,37 +1,38 @@
 'use client';
 
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { Suspense, useRef } from 'react';
-import { Mesh } from 'three';
+import { Canvas } from '@react-three/fiber';
+import { Center, MeshTransmissionMaterial, OrbitControls, Text3D } from '@react-three/drei';
+import { Suspense } from 'react';
+import { useControls } from 'leva';
 import { Scene } from './Scene';
-
-function SpinningBox() {
-  const meshRef = useRef<Mesh>(null);
-
-  useFrame((_state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta;
-      meshRef.current.rotation.y += delta * 0.5;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <icosahedronGeometry args={[10, 2]} />
-      <meshStandardMaterial color='orange' />
-    </mesh>
-  );
-}
+import { MouseTiltWrapper } from '../common/MouseTiltWrapper';
 
 export function HeroSection() {
+  const text3DProps = useControls('Text3D', {
+    size: { value: 50, min: 1, max: 100, step: 1 },
+    height: { value: 5, min: 0.1, max: 10, step: 0.1 },
+    curveSegments: { value: 24, min: 1, max: 50, step: 1 },
+    bevelEnabled: true,
+    bevelThickness: { value: 0.2, min: 0, max: 20, step: 0.01 },
+    bevelSize: { value: 0.1, min: 0, max: 5, step: 0.01 },
+    bevelSegments: { value: 6, min: 1, max: 20, step: 1 },
+  });
+
+  const materialProps = useControls('Material', {
+    backsideThickness: { value: 0.4, min: 0, max: 10, step: 0.5 },
+    thickness: { value: 0, min: 0, max: 10, step: 0.01 },
+  });
+
+  const text = 'Nathan';
+
   return (
-    <div id='canvas-container' className='h-200 border border-gray-500'>
+    <div id='canvas-container' className='h-200'>
       <Canvas orthographic camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 50 }}>
         <Suspense fallback={null}>
           <OrbitControls />
-          <ambientLight intensity={0.3} />
-          <Scene />
+
+          <ambientLight intensity={0.5} />
+
           <directionalLight
             position={[5, 5, 5]}
             intensity={1}
@@ -39,7 +40,31 @@ export function HeroSection() {
             shadow-mapSize-width={1024}
             shadow-mapSize-height={1024}
           />
-          <SpinningBox />
+
+          <MouseTiltWrapper>
+            <Center position={[0, 0, 20]}>
+              <Text3D
+                font='/fonts/Chewy_Regular.json'
+                size={text3DProps.size}
+                height={text3DProps.height}
+                curveSegments={text3DProps.curveSegments}
+                bevelEnabled={text3DProps.bevelEnabled}
+                bevelThickness={text3DProps.bevelThickness}
+                bevelSize={text3DProps.bevelSize}
+                bevelSegments={text3DProps.bevelSegments}
+              >
+                {text}
+                <MeshTransmissionMaterial
+                  backside
+                  backsideThickness={materialProps.backsideThickness}
+                  thickness={materialProps.thickness}
+                  color={'#FF718F'}
+                />
+              </Text3D>
+            </Center>
+
+            <Scene />
+          </MouseTiltWrapper>
         </Suspense>
       </Canvas>
     </div>
--- components/HeroSection/Scene.tsx
+++ components/HeroSection/Scene.tsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react';
 import { Group, Object3DEventMap } from 'three';
-import Fireflies from './Fireflies';
 import { ImagePlane } from '../common/ImagePlane';
 
 export function Scene() {
@@ -8,8 +7,7 @@ export function Scene() {
 
   return (
     <group ref={group}>
-      <ImagePlane url='/bg.jpg' position={[0, 0, 0]} size={[320, 240]} />
-      <Fireflies count={20} radius={80} colors={['orange']} />
+      <ImagePlane url='/images/grid-bg.jpg' position={[0, 0, 0]} size={[320, 240]} />
     </group>
   );
 }
