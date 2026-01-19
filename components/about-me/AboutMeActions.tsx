37c9635f709868ea0df7517c2ab6917a9061dd66
Nathan Ngo <ngothuandeveloper@gmail.com>
2026-01-19 15:52:18 +0700

    feat: remove DocumentDownloadIcon and associated Resume link from About Me section

--- components/about-me/AboutMeActions.tsx
+++ components/about-me/AboutMeActions.tsx
@@ -1,31 +1,18 @@
-import { DocumentDownloadIcon } from '@/icons/DocumentDownloadIcon';
 import { useSmileStore } from '@/stores/smile.store';
 import { AppLink } from '../common/button/AppLink';
-import { ExternalLink } from '../common/button/ExternalLink';
 
 export function AboutMeActions() {
   const { setSmiling } = useSmileStore();
 
   return (
-    <>
-      <AppLink
-        className='px-5 text-lg'
-        href='#my-work'
-        scrollOffset={100}
-        onMouseEnter={() => setSmiling(true)}
-        onMouseLeave={() => setSmiling(false)}
-      >
-        My Work
-      </AppLink>
-
-      <ExternalLink
-        href='/pdfs/Nathan_Ngo_Frontend_Engineer_React.pdf'
-        icon={<DocumentDownloadIcon />}
-        onMouseEnter={() => setSmiling(true)}
-        onMouseLeave={() => setSmiling(false)}
-      >
-        Resume
-      </ExternalLink>
-    </>
+    <AppLink
+      className='px-5 text-lg'
+      href='#my-work'
+      scrollOffset={100}
+      onMouseEnter={() => setSmiling(true)}
+      onMouseLeave={() => setSmiling(false)}
+    >
+      My Work
+    </AppLink>
   );
 }
